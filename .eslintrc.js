9110fbc19084173d1ba375156648003733c82336
walther <waltherpires@hotmail.com>
2024-03-31 00:12:50 -0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'airbnb/hooks',
+    'airbnb',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/prop-types': 0,
+    'no-console': 1,
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  plugins: ['prettier', 'react-hooks', 'react', 'import', 'jsx-a11y'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  singleQuote: true,
+  trailingComma: "es5",
+};
+
+module.exports = config;
+
+
