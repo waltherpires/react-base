45b73535185de50d97da09fff1fb16dfe4513c9b
walther <waltherpires@hotmail.com>
2024-03-31 20:35:37 -0300

    history

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
+import history from './services/history';
 import Routes from './routes';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/Header';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Header />
       <Routes />
       <GlobalStyle />
-    </BrowserRouter>
+    </Router>
   );
 }
 
