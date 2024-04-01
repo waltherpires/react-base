6911c4f2bafd4c35b19ac0703d570c3f00984c67
walther <waltherpires@hotmail.com>
2024-03-31 23:15:58 -0300

    exampleReducer

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,15 +3,14 @@ import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
+import * as exampleActions from '../../store/modules/example/actions';
 
 export default function Login() {
   const dispatch = useDispatch();
   function handleClick(e) {
     e.preventDefault();
 
-    dispatch({
-      type: 'BOTAO_CLICADO',
-    });
+    dispatch(exampleActions.clicaBotao());
   }
 
   return (
