28367725aa9333ad127b333b85f3c5d8fe80d35a
walther <waltherpires@hotmail.com>
2024-03-31 23:59:31 -0300

    redux saga

--- src/store/modules/example/reducer.js
+++ src/store/modules/example/reducer.js
@@ -1,3 +1,5 @@
+import * as types from '../types';
+
 const initialState = {
   botaoClicado: false,
 };
@@ -5,10 +7,21 @@ const initialState = {
 // eslint-disable-next-line default-param-last
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case types.BOTAO_CLICADO_SUCCESS: {
+      console.log('Sucesso');
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
+    }
+
+    case types.BOTAO_CLICADO_FAILURE: {
+      console.log('Deu erro =(');
+      return state;
+    }
+
+    case types.BOTAO_CLICADO_REQUEST: {
+      console.log('Estou fazendo a requisição');
+      return state;
     }
 
     default: {
