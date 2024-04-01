730eecc6ac6b431b66993c26f1bec8d7c53d83b3
walther <waltherpires@hotmail.com>
2024-03-31 22:13:40 -0300

    redux e reducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import history from './services/history';
 import Routes from './routes';
 import GlobalStyle from './styles/GlobalStyles';
@@ -9,12 +11,14 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <Router history={history}>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-      <ToastContainer autoClose={3000} className="toast-container" />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} className="toast-container" />
+      </Router>
+    </Provider>
   );
 }
 
